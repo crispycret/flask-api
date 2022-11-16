d5a19044db5a151c22fb7f99e3c1172d4e5d474d
crispycret <bnadeau.dev@gmail.com>
2022-11-15 20:36:40 -0500

    seperated out social functions from auth blueprint and isolated in social blueprint

--- config.py
+++ config.py
@@ -13,12 +13,19 @@ _summary_ = """
         [+] Build a tool to help the application developer create and manage SECRET_KEY generation. 
      
 """
+
+__SQLALCHEMY_DATABASE_URI__ = os.getenv("DATABASE_URI") or \
+    'sqlite:///' + os.path.join(basedir, 'default.db')
+
+if __SQLALCHEMY_DATABASE_URI__.startswith("postgres://"):
+    __SQLALCHEMY_DATABASE_URI__ = __SQLALCHEMY_DATABASE_URI__.replace("postgres://", "postgresql://", 1)
+
 class Configuration (object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     ADMIN_SECRET_KEY = os.environ.get('ADMIN_SECRET_KEY')
 
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
-        'sqlite:///' + os.path.join(basedir, 'default.db')
-
+    SQLALCHEMY_DATABASE_URI = __SQLALCHEMY_DATABASE_URI__
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    
     
--- core/auth/__init__.py
+++ core/auth/__init__.py
@@ -4,5 +4,7 @@ from flask import Blueprint
 
 auth = Blueprint('auth', __name__, url_prefix='/auth')
 
-from . import routes
+from . import utils
 from . import models
+from . import routes
+from . import decorators
